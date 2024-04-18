396a291bc2bc13af6784ff6be41adddfb09c2b38
Ehsan Gazar <me@ehsangazar.com>
2024-04-18 20:34:45 +0100

    fix: rename to push

--- src/services/EmailQueueService.ts
+++ src/services/EmailQueueService.ts
@@ -11,7 +11,7 @@ class EmailQueueService {
     this.items = [];
     this.emailServiceObj = emailServiceObj;
   }
-  add(item: Item) {
+  push(item: Item) {
     this.items.push(item);
   }
 
--- src/services/__tests__/EmailQueueService.test.ts
+++ src/services/__tests__/EmailQueueService.test.ts
@@ -1,26 +1,64 @@
 import EmailQueueService from "../EmailQueueService";
+import EmailService from "../EmailService";
 
+jest.useFakeTimers();
+jest.mock("../EmailService");
 describe("EmailQueueService", () => {
   it("should be a function", () => {
     expect(typeof EmailQueueService).toBe("function");
   });
   it("should be a function", () => {
-    const EmailQueueServiceObj = new EmailQueueService();
-    expect(typeof EmailQueueServiceObj.add).toBe("function");
-    expect(typeof EmailQueueServiceObj.shift).toBe("function");
-    expect(typeof EmailQueueServiceObj.list).toBe("function");
+    const emailServiceObj = new EmailService();
+    const emailQueueServiceObj = new EmailQueueService(emailServiceObj);
+    expect(typeof emailQueueServiceObj.push).toBe("function");
+    expect(typeof emailQueueServiceObj.shift).toBe("function");
+    expect(typeof emailQueueServiceObj.list).toBe("function");
   });
 
-  it("should be able to add", () => {
-    const emailQueueObj = new EmailQueueService();
-    emailQueueObj.add(1);
-    expect(emailQueueObj.list().length).toBe(1);
+  it("should be able to push", () => {
+    const emailServiceObj = new EmailService();
+    const emailQueueServiceObj = new EmailQueueService(emailServiceObj);
+    emailQueueServiceObj.push(1);
+    expect(emailQueueServiceObj.list().length).toBe(1);
   });
 
   it("should shift the first item", () => {
-    const emailQueueObj = new EmailQueueService();
-    emailQueueObj.add(10);
-    emailQueueObj.add(1);
-    expect(emailQueueObj.shift()).toBe(10);
+    const emailServiceObj = new EmailService();
+    const emailQueueServiceObj = new EmailQueueService(emailServiceObj);
+    emailQueueServiceObj.push(10);
+    emailQueueServiceObj.push(1);
+    expect(emailQueueServiceObj.shift()).toBe(10);
+  });
+
+  it("should start/stop the process", () => {
+    const emailServiceObj = new EmailService();
+    const emailQueueServiceObj = new EmailQueueService(emailServiceObj);
+    emailQueueServiceObj.start();
+    expect(emailQueueServiceObj.started).toBe(true);
+    emailQueueServiceObj.stop();
+    expect(emailQueueServiceObj.started).toBe(false);
+  });
+
+  it("should finish the stack", () => {
+    const emailServiceObj = new EmailService();
+    const emailQueueServiceObj = new EmailQueueService(emailServiceObj);
+    emailServiceObj.sendEmail = jest.fn(async () => true);
+    emailQueueServiceObj.push(1);
+    emailQueueServiceObj.start();
+    setTimeout(() => {
+      expect(emailQueueServiceObj.list().length).toBe(0);
+    }, 10);
+  });
+  it("should finish push an item back to stack", () => {
+    const emailServiceObj = new EmailService();
+    const emailQueueServiceObj = new EmailQueueService(emailServiceObj);
+    emailServiceObj.sendEmail = jest.fn(async () => false);
+    emailQueueServiceObj.push = jest.fn(() => {});
+    emailQueueServiceObj.push(1);
+    emailQueueServiceObj.start();
+    setTimeout(() => {
+      expect(emailQueueServiceObj.push).toHaveBeenCalled();
+      expect(emailQueueServiceObj.list().length).toBe(1);
+    }, 10);
   });
 });
