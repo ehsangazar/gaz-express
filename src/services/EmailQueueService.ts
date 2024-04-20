e0c6bf73385f275fb8142154e7f23eb70293693a
Ehsan Gazar <me@ehsangazar.com>
2024-04-20 11:21:24 +0100

    fix: refactoring EmailQueueService

--- src/services/EmailQueueService.ts
+++ src/services/EmailQueueService.ts
@@ -1,58 +1,27 @@
-import config from "../config/config";
-
-interface Item {}
-
-class EmailQueueService {
-  private items: Item[];
-  private callToFunction: (item: Item) => Promise<boolean>;
-  public started: boolean = false;
-
-  constructor({
-    callToFunction,
-  }: {
-    callToFunction: (item: Item) => Promise<boolean>;
-  }) {
+class EmailQueueService<T> {
+  private items: T[];
+  constructor() {
     this.items = [];
-    this.callToFunction = callToFunction;
   }
-  push(item: Item) {
+
+  enqueue(item: T): void {
     this.items.push(item);
   }
 
-  shift() {
+  dequeue(): T | undefined {
     return this.items.shift();
   }
 
-  list() {
-    return this.items;
+  isEmpty(): boolean {
+    return this.items.length === 0;
   }
 
-  private loopProcessing() {
-    setTimeout(() => {
-      this.processing();
-    }, config.DELAY_QUEUE_EMAIL);
+  size(): number {
+    return this.items.length;
   }
 
-  private async processing() {
-    if (this.start) {
-      const item = this.items[0];
-      if (!item) {
-        this.loopProcessing();
-        return;
-      }
-      const response = await this.callToFunction(item);
-      if (response) this.shift();
-      this.loopProcessing();
-    }
-  }
-
-  start() {
-    this.started = true;
-    this.processing();
-  }
-
-  stop() {
-    this.started = false;
+  clear(): void {
+    this.items = [];
   }
 }
 
--- src/services/__tests__/EmailQueueService.test.ts
+++ src/services/__tests__/EmailQueueService.test.ts
@@ -7,67 +7,37 @@ describe("EmailQueueService", () => {
     expect(typeof EmailQueueService).toBe("function");
   });
   it("should be a function", () => {
-    const emailServiceObj = new EmailService();
-    const emailQueueServiceObj = new EmailQueueService({
-      callToFunction: emailServiceObj.sendEmail,
-    });
-    expect(typeof emailQueueServiceObj.push).toBe("function");
-    expect(typeof emailQueueServiceObj.shift).toBe("function");
-    expect(typeof emailQueueServiceObj.list).toBe("function");
+    const emailQueueServiceObj = new EmailQueueService();
+    expect(typeof emailQueueServiceObj.enqueue).toBe("function");
+    expect(typeof emailQueueServiceObj.dequeue).toBe("function");
+    expect(typeof emailQueueServiceObj.isEmpty).toBe("function");
+    expect(typeof emailQueueServiceObj.size).toBe("function");
+    expect(typeof emailQueueServiceObj.clear).toBe("function");
   });
 
   it("should be able to push", () => {
-    const emailServiceObj = new EmailService();
-    const emailQueueServiceObj = new EmailQueueService({
-      callToFunction: emailServiceObj.sendEmail,
-    });
-    emailQueueServiceObj.push(1);
-    expect(emailQueueServiceObj.list().length).toBe(1);
+    const emailQueueServiceObj = new EmailQueueService();
+    emailQueueServiceObj.enqueue(1);
+    expect(emailQueueServiceObj.size()).toBe(1);
   });
 
   it("should shift the first item", () => {
-    const emailServiceObj = new EmailService();
-    const emailQueueServiceObj = new EmailQueueService({
-      callToFunction: emailServiceObj.sendEmail,
-    });
-    emailQueueServiceObj.push(10);
-    emailQueueServiceObj.push(1);
-    expect(emailQueueServiceObj.shift()).toBe(10);
+    const emailQueueServiceObj = new EmailQueueService();
+    emailQueueServiceObj.enqueue(10);
+    emailQueueServiceObj.enqueue(1);
+    expect(emailQueueServiceObj.dequeue()).toBe(10);
   });
 
-  it("should start/stop the process", () => {
-    const emailServiceObj = new EmailService();
-    const emailQueueServiceObj = new EmailQueueService({
-      callToFunction: emailServiceObj.sendEmail,
-    });
-    emailQueueServiceObj.start();
-    expect(emailQueueServiceObj.started).toBe(true);
-    emailQueueServiceObj.stop();
-    expect(emailQueueServiceObj.started).toBe(false);
+  it("should return undefined if queue is empty", () => {
+    const emailQueueServiceObj = new EmailQueueService();
+    expect(emailQueueServiceObj.dequeue()).toBe(undefined);
   });
 
-  it("should finish the stack", async () => {
-    const emailServiceObj = new EmailService();
-    emailServiceObj.sendEmail = jest.fn(async () => true);
-    const emailQueueServiceObj = new EmailQueueService({
-      callToFunction: emailServiceObj.sendEmail,
-    });
-    emailQueueServiceObj.push(1);
-    emailQueueServiceObj.start();
-    await new Promise((r) => setTimeout(r, 10));
-    emailQueueServiceObj.stop();
-    expect(emailQueueServiceObj.list().length).toBe(0);
-  });
-  it("should finish push an item back to stack", async () => {
-    const emailServiceObj = new EmailService();
-    emailServiceObj.sendEmail = jest.fn(async () => false);
-    const emailQueueServiceObj = new EmailQueueService({
-      callToFunction: emailServiceObj.sendEmail,
-    });
-    emailQueueServiceObj.push(1);
-    emailQueueServiceObj.start();
-    await new Promise((r) => setTimeout(r, 100));
-    emailQueueServiceObj.stop();
-    expect(emailQueueServiceObj.list().length).toBe(1);
+  it("should clear the queue", () => {
+    const emailQueueServiceObj = new EmailQueueService();
+    emailQueueServiceObj.enqueue(10);
+    emailQueueServiceObj.enqueue(1);
+    emailQueueServiceObj.clear();
+    expect(emailQueueServiceObj.size()).toBe(0);
   });
 });
